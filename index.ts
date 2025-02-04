fcff9cc606ad3d59bc89af414333d48be39d78c8
Sam Roehrich <sroehrich@outsideinc.com>
2025-02-03 18:35:32 -0700

    feat: moar stuff

--- index.ts
+++ index.ts
@@ -1,42 +1,15 @@
-import lighthouse from "lighthouse";
-import * as chromeLauncher from "chrome-launcher";
-
-import homepage from "./homepage.html";
-
-const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
-const options = {
-    logLevel: "info",
-    output: "html",
-    onlyCategories: ["performance"],
-    port: chrome.port,
-};
+import homepage from "@@/html/homepage.html";
+import pageSpeed from "@@/html/page-speed.html";
+import blogPage from "@@/html/blog.html";
 
 const server = Bun.serve({
     static: {
         "/": homepage,
+        "/blog": blogPage,
+        "/page-speed": pageSpeed,
     },
-    async fetch(request, server) {
-        if (request.url.endsWith("/test")) {
-            const fd = await request.formData();
-            const site = fd.get("site");
-            if (!site) return new Response("must provide a site");
-            console.time("runner");
-            // @ts-ignore
-            const runnerResult = await lighthouse(site.toString(), options);
-            console.timeEnd("runner");
-
-            if (
-                !runnerResult?.report ||
-                typeof runnerResult.report === "object"
-            )
-                return new Response("failed to run report");
-            return new Response(runnerResult.report, {
-                headers: {
-                    "Content-Type": "text/html",
-                },
-            });
-        }
-        return new Response("404");
+    async fetch(req, server) {
+        return new Response("hi from bun");
     },
 });
 
